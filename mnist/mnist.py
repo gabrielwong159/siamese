e6d365f76f93255e40dca684f3b2c6ffeb81f41f
gabrielwong159 <gabrielwong159@gmail.com>
2018-07-17 22:18:33 +0800

    Implement loss with spring for MNIST as well [acc=0.9899]

--- mnist/mnist.py
+++ mnist/mnist.py
@@ -1,35 +1,30 @@
 import numpy as np
 import cv2
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
 from tensorflow.examples.tutorials.mnist import input_data
 from os.path import join
-from tqdm import trange
+from tqdm import tqdm, trange
 from siamese import Siamese
 
 mnist = input_data.read_data_sets('data/MNIST_data/', one_hot=False)
 model_path = 'model/siamese/model'
+h, w, c = 28, 28, 1
 
 
 def train():
     learning_rate = 1e-4
-    num_iterations = 10_000
+    num_iterations = 20_000
 
     siamese = Siamese()
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    train_step = optimizer.minimize(siamese.loss)
+
+    saver = tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        variables_to_restore = slim.get_variables_to_restore(include=['siamese/conv'])
-        restorer = tf.train.Saver(variables_to_restore)
-        restorer.restore(sess, 'model/classifier/model')
 
-        optimizer = tf.train.AdamOptimizer(learning_rate)
-        train_step = optimizer.minimize(siamese.loss)
-        sess.run(tf.variables_initializer(optimizer.variables()))
-
-        saver = tf.train.Saver()
-
-        for i in range(num_iterations):
+        for i in trange(num_iterations):
             x1, y1 = mnist.train.next_batch(128)
             x2, y2 = mnist.train.next_batch(128)
 
@@ -48,10 +43,10 @@ def train():
             assert not np.isnan(loss_v), 'Model diverged with loss = NaN'
 
             if i % 100 == 0:
-                print(f'step {i}: loss {loss_v}')
+                tqdm.write(f'step {i}: loss {loss_v}')
 
             if i % 1000 == 0:
-                print('Model saved:', saver.save(sess, model_path))
+                tqdm.write('Model saved:', saver.save(sess, model_path))
 
         print('Finished:', saver.save(sess, model_path))
 
@@ -59,8 +54,8 @@ def train():
 def test():
     test_data = 'data/labels'
     files = [join(test_data, f'{i}.png') for i in range(10)]
-    truth = [cv2.imread(f, 0) / 255 for f in files]
-    truth = np.array(truth).reshape([-1, 28, 28, 1])
+    ground = [cv2.imread(f, 0) / 255 for f in files]
+    ground = np.array(ground).reshape([-1, 28, 28, 1])
 
     siamese = Siamese()
     saver = tf.train.Saver()
@@ -68,28 +63,33 @@ def test():
     with tf.Session() as sess:
         saver.restore(sess, model_path)
 
-        x, y = mnist.test.images, mnist.test.labels
-        x = np.reshape(x, [-1, 28, 28, 1])
-        n = 0
-        with trange(len(x)) as pbar:
-            for i in pbar:
-                image, label = x[i], y[i]
-                feed_dict = {
-                    siamese.x1: np.array([image for _ in truth]),
-                    siamese.x2: truth,
-                    siamese.keep_prob: 1.0,
-                }
-                sigmoid = sess.run([siamese.out], feed_dict=feed_dict)
-                pred = np.argmax(sigmoid)
-                n += (pred == label)
+        ground_scores = sess.run(siamese.o1, feed_dict={
+            siamese.x1: ground,
+            siamese.keep_prob: 1.0,
+        })
 
-                if (i+1) % 100 == 0:
-                    pbar.set_postfix(acc=n/i)
-        print(n / i)
+        x, y = mnist.test.images, mnist.test.labels
+        x = np.reshape(x, [-1, h, w, c])
+
+        n_correct = 0
+        batch_size = 1000
+        n_batches = len(x) // batch_size
+        for i in trange(n_batches):
+            batch_images, batch_labels = x[i::n_batches], y[i::n_batches]
+            batch_scores = sess.run(siamese.o1, feed_dict={
+                siamese.x1: batch_images,
+                siamese.keep_prob: 1.0,
+            })
+            for score, label in zip(batch_scores, batch_labels):
+                dist = np.sum(np.abs(ground_scores - score), axis=-1)
+                pred = np.argmin(dist)
+                n_correct += (pred == label)
+
+        print(n_correct / len(x))
 
 
 if __name__ == '__main__':
-    # with tf.Graph().as_default():
-    #    train()
+    #with tf.Graph().as_default():
+    #   train()
     with tf.Graph().as_default():
-        test()
+       test()
--- mnist/siamese.py
+++ mnist/siamese.py
@@ -1,9 +1,7 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
-h = 28
-w = 28
-c = 1
+h, w, c = 28, 28, 1
 
 
 class Siamese(object):
@@ -13,34 +11,37 @@ class Siamese(object):
         self.y_ = tf.placeholder(tf.float32, [None])
         self.keep_prob = tf.placeholder(tf.float32)
 
-        logits = self.network(self.x1, self.x2)
-        labels = tf.expand_dims(self.y_, axis=-1)
-        cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits)
-        self.loss = tf.reduce_mean(cross_entropy)
-        self.out = tf.nn.sigmoid(logits)
-
-    def network(self, x1, x2):
         with tf.variable_scope('siamese') as scope:
-            o1 = self.convnet(x1)
+            self.o1 = self.network(self.x1)
             scope.reuse_variables()
-            o2 = self.convnet(x2)
+            self.o2 = self.network(self.x2)
+        self.loss = self.loss_with_spring()
 
-        dist = tf.abs(o1 - o2)
-        logits = slim.fully_connected(dist, 1, activation_fn=None)
-        return logits
+    def network(self, x):
+        net = slim.conv2d(x, 32, [3, 3], scope='conv1')
+        net = slim.max_pool2d(net, [2, 2], scope='pool1')
 
-    def convnet(self, x):
-        with slim.arg_scope([slim.conv2d], trainable=False):
-            net = slim.conv2d(x, 32, [5, 5], scope='conv1')
-            net = slim.max_pool2d(net, [2, 2], scope='pool1')
-
-            net = slim.conv2d(net, 64, [5, 5], scope='conv2')
-            net = slim.max_pool2d(net, [2, 2], scope='pool2')
+        net = slim.conv2d(net, 64, [3, 3], scope='conv2')
+        net = slim.max_pool2d(net, [2, 2], scope='pool2')
 
         net = slim.flatten(net, scope='flat')
 
         net = slim.fully_connected(net, 1024, scope='fc1')
-        net = slim.dropout(net, keep_prob=0.5, scope='drop1')
+        net = slim.dropout(net, keep_prob=self.keep_prob, scope='drop1')
 
-        net = slim.fully_connected(net, 1024, activation_fn=tf.nn.sigmoid, scope='fc2')
+        net = slim.fully_connected(net, 1024, activation_fn=None, scope='fc2')
         return net
+
+    def loss_with_spring(self):
+        margin = 5.0
+        labels_t = self.y_
+        labels_f = tf.subtract(1.0, self.y_, name="1-yi")
+        eucd2 = tf.pow(tf.subtract(self.o1, self.o2), 2)
+        eucd2 = tf.reduce_sum(eucd2, 1)
+        eucd = tf.sqrt(eucd2+1e-6, name="eucd")
+        C = tf.constant(margin, name="C")
+        pos = tf.multiply(labels_t, eucd2, name="yi_x_eucd2")
+        neg = tf.multiply(labels_f, tf.pow(tf.maximum(tf.subtract(C, eucd), 0), 2), name="Nyi_x_C-eucd_xx_2")
+        losses = tf.add(pos, neg, name="losses")
+        loss = tf.reduce_mean(losses, name="loss")
+        return loss
